b23289c3a6fde1d9f64c35903b217687259c9e43
Sana Ullah <mr.ssk998@gmail.com>
2024-04-26 21:01:38 +0500

    Header Updated

--- src/assets/Components/Header.jsx
+++ src/assets/Components/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Header = (props) => {
+  let {topic, level, totalQuestions, scorePerQuestion} = props;
   return (
     <>
       <h1 className="bg-dark text-light text-center rounded-3 p-2 my-3">
@@ -9,16 +10,16 @@ const Header = (props) => {
       <div className="mb-3 bg-info-subtle rounded-3">
         <div className="row row-cols-2 p-2">
           <div className="col">
-            <p className="my-1">Topic: {props.topic}</p>
+            <p className="my-1">Topic: {topic}</p>
           </div>
           <div className="col">
-            <p className="my-1">Level: {props.level}</p>
+            <p className="my-1">Level: {level}</p>
           </div>
           <div className="col">
-            <p className="my-1">Total Questions: {props.totalQuestions}</p>
+            <p className="my-1">Total Questions: {totalQuestions}</p>
           </div>
           <div className="col">
-            <p className="my-1">Score Per Question: {props.scorePerQuestion}</p>
+            <p className="my-1">Score Per Question: {scorePerQuestion}</p>
           </div>
         </div>
       </div>
